e0cac5b1cf07c5cb13c991c7a2e2e82706fd08e5
ayanokoujifl <guleite3@gmail.com>
2023-01-29 14:46:23 -0300

    SalesCard and Header components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import './style.css'
+import Logo from '../../assets/img/logo.svg'
+
+export function Header() {
+  return (
+    <>
+      <header>
+        <div className="dsmeta-logo-container">
+          <img src={Logo} alt="DSMeta" />
+          <h1>DSMeta</h1>
+          <p>
+            Desenvolvido por
+            <a href="https://www.instagram.com/luis_gustavo_dev">
+              @luis_gustavo_dev
+            </a>
+          </p>
+        </div>
+      </header>
+    </>
+  )
+}
